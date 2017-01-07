fbe325f5d00b44addb6973f12744ec62e73b2e30
Sina Sadeghi <sina.sa@gmail.com>
2017-01-07 17:31:24 +1100

    add start_time to schedule

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ What you should be doing with your life.
  * Install GNU Prolog: `dnf install gprolog`
 
 ## Usage
- * Compile with: `consult('lifebuddy.pl').
- * See the schedule with: `schedule(X, monday).`, pick any day of the week.
+ * Compile with: `consult('lifebuddy.pl').`.
+ * See the schedule with: `schedule(X, Y, monday).`, pick any day of the week.
+ * See what tasks are scheduled for during a particular hour with: `schedule(X, start_time(17, _, _), monday).`.
+ * Check when a particular task is scheduled with: `schedule(laundry,X,Y).`.
--- lifebuddy.pl
+++ lifebuddy.pl
@@ -17,20 +17,21 @@ weekend(sunday).
 
 schoolnight(Day) :- weekday(Day), Day \= friday, Day \= saturday.
 
-schedule(wakeup, Day) :- weekday(Day).
-schedule(exercise_morning, Day) :- weekday(Day), Day \= saturday.
-schedule(brush_teeth_morning, Day) :- weekday(Day).
-schedule(shower, Day) :- weekday(Day).
-schedule(breakfast, Day) :- weekday(Day).
-schedule(travel_to_work, Day) :- workday(Day).
-schedule(laundry, sunday).
-schedule(work_before_lunch, Day) :- workday(Day).
-schedule(lunch, Day) :- weekday(Day).
-schedule(work_after_lunch, Day) :- workday(Day).
-schedule(travel_from_work, Day) :- workday(Day).
-schedule(exercise_afternoon, Day) :- schoolnight(Day).
-schedule(dinner, Day) :- weekday(Day).
-schedule(prepare_for_next_day, Day) :- schoolnight(Day).
-schedule(leisure, Day) :- schoolnight(Day).
-schedule(brush_teeth_evening, Day) :- weekday(Day).
-schedule(slumber, Day) :- weekday(Day).
+schedule(wakeup, start_time(5,0,0), Day) :- weekday(Day).
+schedule(exercise_morning, start_time(5,5,0), Day) :- weekday(Day), Day \= saturday.
+schedule(brush_teeth_morning, start_time(6,0,0), Day) :- weekday(Day).
+schedule(shower, start_time(6,5,0), Day) :- weekday(Day).
+schedule(breakfast, start_time(6,20,0), Day) :- weekday(Day).
+schedule(travel_to_work, start_time(7,0,0), Day) :- workday(Day).
+schedule(laundry, start_time(11,0,0), sunday).
+schedule(work_before_lunch, start_time(8,0,0), Day) :- workday(Day).
+schedule(lunch, start_time(12,0,0), Day) :- weekday(Day).
+schedule(work_after_lunch, start_time(12,45,0), Day) :- workday(Day).
+schedule(travel_from_work, start_time(16,15,0), Day) :- workday(Day).
+schedule(exercise_afternoon, start_time(17,15,0), Day) :- schoolnight(Day).
+schedule(dinner, start_time(18,15,0), Day) :- weekday(Day).
+schedule(prepare_for_next_day, start_time(19,15,0), Day) :- schoolnight(Day).
+schedule(leisure, start_time(20,15,0), Day) :- schoolnight(Day).
+schedule(brush_teeth_evening, start_time(20,30,0), Day) :- weekday(Day).
+schedule(prepare_for_bed, start_time(20,35,0), Day) :- weekday(Day).
+schedule(slumber, start_time(21,0,0), Day) :- weekday(Day).
