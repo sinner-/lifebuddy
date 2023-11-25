e84f4190c769c8b35295df8ed79e0b7a255154cd
Sina Sadeghi <sina.sa@gmail.com>
2023-11-25 19:45:52 +1100

    better version

--- lifebuddy.pl
+++ lifebuddy.pl
@@ -1,150 +1,74 @@
-weekday(monday).
-weekday(tuesday).
-weekday(wednesday).
-weekday(thursday).
-weekday(friday).
-weekday(saturday).
-weekday(sunday).
+% prolog
 
-workday(monday).
-workday(tuesday).
-workday(wednesday).
-workday(thursday).
-workday(friday).
+day(monday).
+day(tuesday).
+day(wednesday).
+day(thursday).
+day(friday).
+day(saturday).
+day(sunday).
 
-weekend(saturday).
-weekend(sunday).
+activity(brush_teeth).
+activity(exercise).
+activity(work_morning).
+activity(work_afternoon).
+activity(date_night).
+activity(sleep).
+activity(visit_family).
+activity(computer_chores).
+activity(breakfast).
+activity(lunch).
+activity(dinner).
+
+workday(Day) :-
+  Day \= saturday,
+  Day \= sunday.
+
+weekend(Day) :-
+  \+ workday(Day).
 
 schoolnight(Day) :-
-  weekday(Day),
+  day(Day),
   Day \= friday,
   Day \= saturday.
 
-return_time(Minutes, Time) :-
-  get_time(CurrentTime),
-  stamp_date_time(CurrentTime, CurrentTimeStamp, local),
-  date_time_value(year, CurrentTimeStamp, Year),
-  date_time_value(month, CurrentTimeStamp, Month),
-  date_time_value(day, CurrentTimeStamp, Day),
-  date_time_stamp(date(Year, Month, Day, 0, Minutes, 0, 0, -, -), Stamp),
-  stamp_date_time(Stamp, D, 0),
-  date_time_value(time, D, Time).
+day_constraint(brush_teeth, Day) :- day(Day).
+day_constraint(sleep, Day) :- day(Day).
+day_constraint(exercise, Day) :- day(Day).
+day_constraint(breakfast, Day) :- day(Day).
+day_constraint(lunch, Day) :- day(Day).
+day_constraint(dinner, Day) :- day(Day).
+day_constraint(date_night, friday).
+day_constraint(visit_family, wednesday).
+day_constraint(work_morning, Day) :- workday(Day).
+day_constraint(work_afternoon, Day) :- workday(Day).
+day_constraint(computer_chores, Day) :- Day = sunday.
 
-schedule(wakeup, TaskLength, TaskStartTime, Day) :- 
-  weekday(Day),
-  TaskLength is 0,
-  TaskStartTime is 300.
+duration(brush_teeth, 5).
+duration(sleep, 480).
+duration(exercise, 60).
+duration(breakfast, 30).
+duration(lunch, 30).
+duration(dinner, 30).
+duration(date_night, 120).
+duration(visit_family, 120).
+duration(work_morning, 240).
+duration(work_afternoon, 240).
+duration(computer_chores, 60).
 
-schedule(exercise_morning, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60,
-  schedule(wakeup, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
+next(brush_teeth, exercise).
+next(exercise, breakfast).
+next(breakfast, work_morning).
 
-schedule(brush_teeth_morning, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 10,
-  schedule(exercise_morning, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
+time_constraint(brush_teeth, 420).
+time_constraint(Activity, Timeslot) :-
+  next(PreviousActivity, Activity),
+  duration(PreviousActivity, Duration),
+  time_constraint(PreviousActivity, PreviousTimeslot),
+  Timeslot is Duration + PreviousTimeslot.
 
-schedule(brush_teeth_morning, TaskLength, TaskStartTime, Day) :-
-  weekend(Day),
-  TaskLength is 5,
-  schedule(wakeup, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(shower, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 15,
-  schedule(brush_teeth_morning, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(shower, TaskLength, TaskStartTime, Day) :-
-  weekend(Day),
-  TaskLength is 20,
-  schedule(brush_teeth_morning, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(breakfast, TaskLength, TaskStartTime, Day) :-
-  weekday(Day),
-  TaskLength is 30,
-  schedule(shower, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(travel_to_work, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60,
-  schedule(breakfast, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(laundry, TaskLength, TaskStartTime, Day) :-
-  Day == sunday,
-  TaskLength is 5,
-  schedule(breakfast, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(work_before_lunch, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60 * 3.5,
-  schedule(travel_to_work, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(lunch, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 45,
-  schedule(work_before_lunch, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(work_after_lunch, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60 * 4,
-  schedule(lunch, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(travel_from_work, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60,
-  schedule(work_after_lunch, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(exercise_afternoon, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60,
-  schedule(travel_from_work, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(dinner, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60,
-  schedule(exercise_afternoon, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(prepare_for_next_day, TaskLength, TaskStartTime, Day) :-
-  workday(Day),
-  TaskLength is 60,
-  schedule(dinner, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(leisure, TaskLength, TaskStartTime, Day) :-
-  schoolnight(Day),
-  TaskLength is 15,
-  schedule(prepare_for_next_day, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(brush_teeth_evening, TaskLength, TaskStartTime, Day) :-
-  schoolnight(Day),
-  TaskLength is 5,
-  schedule(leisure, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(prepare_for_bed, TaskLength, TaskStartTime, Day) :-
-  schoolnight(Day),
-  TaskLength is 30,
-  schedule(brush_teeth_evening, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
-
-schedule(slumber, TaskLength, TaskStartTime, Day) :-
-  schoolnight(Day),
-  TaskLength is 0,
-  schedule(prepare_for_bed, PreviousTaskLength, PreviousTaskStartTime, Day),
-  TaskStartTime is PreviousTaskStartTime + PreviousTaskLength.
+schedule(Day, Schedule) :-
+  bagof([Activity, Timeslot], (
+    day_constraint(Activity, Day),
+    time_constraint(Activity, Timeslot)
+  ), Schedule).
